a15a36eb08c1a3082c8f053dfb4eae664fbb90e5
Guandaru-Kabugi <kabugi96@gmail.com>
2024-06-02 12:10:41 +0300

    Testing new repo

---
+++ band_name.py
@@ -0,0 +1,7 @@
+import time
+
+print("Welcome to the Band Name Generator.")
+time.sleep(1)
+city = input("What is the name of the city you grew up in? ")
+pet = input("What is the name of your pet? ")
+print("Your band name could be " + city + " " + pet)
